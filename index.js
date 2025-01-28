957a27f6577064688e1c5c3b8cb569dfb6c6d8ba
valpryz <valpryz@gmail.com>
2025-01-28 18:05:14 +0100

    exercise 3.8 completed

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 
 app.use(express.json())
+
+morgan.token("output",(request, response) => JSON.stringify(request.body))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :output"))
 
 let phoneNumbers = [
     { 
